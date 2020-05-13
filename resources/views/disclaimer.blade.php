0f05a0d5b28876d763e495c9bf5bf20b1606bf14
Arie Maulana <arie.m@mhs.unsyiah.ac.id>
2020-05-13 10:51:50 +0700

    Disclaimer
    pelindungan bagi pemilik website

---
+++ resources/views/disclaimer.blade.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title> Disclaimer </title>
+</head>
+<body>
+</head>
+<body>
+<h1 align=center>Disclaimer</h1>
+    <hr>
+    
+    <tr>
+        <td> Apabila anda butuh informasi maupun pertanyaan berkaitan dengan isi Disclaimer ini, silahkan kontak kami pada email yang terddapat di Contact Us. </td>  
+    </tr>
+
+
+Seluruh informasi dalam website JakBloe ditujukan hanya sebagai informasi umum. Kami tidak menjamin tentang keakuratan, kepastian dan kebenaran seluruh informasi dan tutorial yang ada. Setiap tindakan yang anda lakukan berdasarkan informasi dari situs ini merupakan tanggung jawab anda sendiri. Kami tidak bertanggung jawab terhadap kerugian materi dan immateri yang ditimbulkan oleh informasi yang tersaji dalam situs ini.
+
+Dari situs JakBloe, terdapat beberapa link keluar untuk mengunjungi situs lain. Walaupun kami selalu berusaha untuk menyediakan link ke situs berkualitas, namun kami tidak bisa mengontrol isi dari situs tersebut. Pemilik situs tersebut bisa saja mengubah konten yang ada tanpa pemberitahuan. Dalam hal ini, www.matericoding.com juga tidak bertanggung jawab terhadap kerugian yang berasal dari situs web tersebut.
+
+Juga perlu diingat bahwa ketika mengunjungi situs lain, situs tersebut bisa saja memiliki kebijakan privasi dan penggunaan informasi yang berbeda. Pastikan anda membaca kebijakan privasi dari situs tersebut dan juga aturan penggunaan informasi sebelum memberikan informasi apapun.
+
+Dengan mengunjungi situs JakBloe ini, anda setuju dengan isi disclaimer dan seluruh aturannya.
+
+Terimakasih telah mengunjungi JakBloe
+
+</body>
+</html>
