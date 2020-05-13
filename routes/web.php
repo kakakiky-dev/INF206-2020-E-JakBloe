112d2e47d599a41eafae92dc55a1efaba8a9a8cd
Nazla Amelia <nazla.a@mhs.unsyiah.ac.id>
2020-05-13 08:21:09 +0700

    About us
    Membuat about us dasar jakbloe

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return view('about');
+});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
