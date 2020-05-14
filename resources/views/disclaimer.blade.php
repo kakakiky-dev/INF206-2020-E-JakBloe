a77963046a110feb369fe7af8ba77bd39d4431bc
Arie Maulana <arie.m@mhs.unsyiah.ac.id>
2020-05-14 23:16:13 +0700

    Update terbaru disclaimer
    menambah tampilan css pada halaman disclaimer

--- resources/views/disclaimer.blade.php
+++ resources/views/disclaimer.blade.php
@@ -1,27 +1,41 @@
-<html>
-<head>
-<title> Disclaimer </title>
-</head>
-<body>
-</head>
-<body>
-<h1 align=center>Disclaimer</h1>
-    <hr>
-    
-    <tr>
-        <td> Apabila anda butuh informasi maupun pertanyaan berkaitan dengan isi Disclaimer ini, silahkan kontak kami pada email yang terddapat di Contact Us. </td>  
-    </tr>
+@extends('master')
 
+@section('deskripsi')
+<section class="breadcrumb_area">
+            <img class="breadcrumb_shap" src="img/breadcrumb/banner_bg.png" alt="">
+            <div class="container">
+                <div class="breadcrumb_content text-center">
+                    <h1 class="f_p f_700 f_size_50 w_color l_height50 mb_20">Privacy Policy</h1>
+                    <p class="f_400 w_color f_size_16 l_height26">Halaman Kebijakan Website JakBloe</p>
+                </div>
+            </div>
+        </section>
 
-Seluruh informasi dalam website JakBloe ditujukan hanya sebagai informasi umum. Kami tidak menjamin tentang keakuratan, kepastian dan kebenaran seluruh informasi dan tutorial yang ada. Setiap tindakan yang anda lakukan berdasarkan informasi dari situs ini merupakan tanggung jawab anda sendiri. Kami tidak bertanggung jawab terhadap kerugian materi dan immateri yang ditimbulkan oleh informasi yang tersaji dalam situs ini.
+        <section class="blog_area sec_pad">
+            <div class="container">
+            <h1>Disclaimer for JakBloe</h1>
 
-Dari situs JakBloe, terdapat beberapa link keluar untuk mengunjungi situs lain. Walaupun kami selalu berusaha untuk menyediakan link ke situs berkualitas, namun kami tidak bisa mengontrol isi dari situs tersebut. Pemilik situs tersebut bisa saja mengubah konten yang ada tanpa pemberitahuan. Dalam hal ini, JakBloe juga tidak bertanggung jawab terhadap kerugian yang berasal dari situs web tersebut.
+<p>Jika Anda memerlukan informasi lebih lanjut atau memiliki pertanyaan tentang penolakan situs kami, jangan ragu untuk menghubungi kami melalui email di jakbloe@gmail.com</p>
 
-Juga perlu diingat bahwa ketika mengunjungi situs lain, situs tersebut bisa saja memiliki kebijakan privasi dan penggunaan informasi yang berbeda. Pastikan anda membaca kebijakan privasi dari situs tersebut dan juga aturan penggunaan informasi sebelum memberikan informasi apapun.
+<h2>Disclaimers for JakBloe</h2>
 
-Dengan mengunjungi situs JakBloe ini, anda setuju dengan isi disclaimer dan seluruh aturannya.
+<p>Semua informasi di situs web ini - www.jakbloe.com - dipublikasikan dengan itikad baik dan hanya untuk tujuan informasi umum. JakBloe tidak memberikan jaminan tentang kelengkapan, keandalan, dan keakuratan informasi ini. Segala tindakan yang Anda ambil atas informasi yang Anda temukan di situs web ini (JakBloe), sepenuhnya merupakan risiko Anda sendiri. JakBloe tidak akan bertanggung jawab atas kerugian dan / atau kerusakan sehubungan dengan penggunaan situs web kami.</p>
 
-Terimakasih telah mengunjungi JakBloe
+<p>Seluruh informasi dalam website JakBloe ditujukan hanya sebagai informasi umum. Kami tidak menjamin tentang keakuratan, kepastian dan kebenaran seluruh informasi dan tutorial yang ada. Setiap tindakan yang anda lakukan berdasarkan informasi dari situs ini merupakan tanggung jawab anda sendiri. Kami tidak bertanggung jawab terhadap kerugian materi dan immateri yang ditimbulkan oleh informasi yang tersaji dalam situs ini.</p>
 
-</body>
-</html>
+<p>Dari situs JakBloe, terdapat beberapa link keluar untuk mengunjungi situs lain. Walaupun kami selalu berusaha untuk menyediakan link ke situs berkualitas, namun kami tidak bisa mengontrol isi dari situs tersebut. Pemilik situs tersebut bisa saja mengubah konten yang ada tanpa pemberitahuan. Dalam hal ini, www.matericoding.com juga tidak bertanggung jawab terhadap kerugian yang berasal dari situs web tersebut.</p>
+
+<p>Juga perlu diingat bahwa ketika mengunjungi situs lain, situs tersebut bisa saja memiliki kebijakan privasi dan penggunaan informasi yang berbeda. Pastikan anda membaca kebijakan privasi dari situs tersebut dan juga aturan penggunaan informasi sebelum memberikan informasi apapun.</p>
+
+<p>Dengan mengunjungi situs JakBloe ini, anda setuju dengan isi disclaimer dan seluruh aturannya.</p>
+
+<h2>Persetujuan</h2>
+
+<p>Dengan menggunakan situs web kami, Anda dengan ini menyetujui penafian kami dan menyetujui ketentuannya.</p>
+
+<h2>Update</h2>
+
+<p>Jika kami memperbarui, mengubah atau membuat perubahan apa pun pada dokumen ini, perubahan itu akan diposting secara jelas di sini.</p>
+</div>
+</section>
+@endsection
