84880e032df345cfb29d7f094f031724764a459d
alghifari3 <ghifari.13@mhs.unsyiah.ac.id>
2020-05-14 18:04:04 +0700

    Update home.blade.php
    Halaman awal dari website

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,66 @@
-@extends('layouts.app')
+@extends('master')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+@section('deskripsi')
+<section class="new_startup_banner_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 wow fadeInLeft" data-wow-delay="0.4s">
+                        <div class="new_startup_img">
+                            <div class="line line_one"><img src="img/new/line_01.png" alt=""></div>
+                            <div class="line line_two"><img src="img/new/line_02.png" alt=""></div>
+                            <div class="line line_three"><img src="img/new/line_03.png" alt=""></div>
+                            <img src="img/new/welcomejb2.png" alt="">
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
+                    <div class="col-lg-6 d-flex align-items-center">
+                        <div class="new_startup_content">
+                            <h2 class="f_700 f_size_40 l_height50 w_color mb_20 wow fadeInRight" data-wow-delay="0.3s">Gabung Bersama<br><span>JakBloe</span></h2>
+                            <p class="f_400 w_color l_height28 wow fadeInRight" data-wow-delay="0.4s">JakBloe adalah sebuah website yang memungkinkan kamu untuk membeli barang secara massal bersama dengan pedagang lainnya, sehingga barang yang kamu beli menjadi lebih murah.</p>
+                            <div class="action_btn d-flex align-items-center mt_40 wow fadeInRight" data-wow-delay="0.6s">
+                                <a href="#" class="btn_hover app_btn">Daftar <i class="ti-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+        </section>
+        <section class="startup_fuatures_area sec_pad">
+            <div class="container">
+                <div class="sec_title mb_70 wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                    <h2 class="f_p f_size_30 l_height40 f_600 t_color text-center">Selamat Datang di Website<br> JakBloe</h2>
+                </div>
+                <ul class="nav nav-tabs startup_tab" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="market-tab" data-toggle="tab" href="#market" role="tab" aria-controls="market" aria-selected="true">
+                            <span class="icon"><i class="icon-cloud-upload"></i></span>
+                            <h3>Marketing<br> Cloud</h3>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="app-tab" data-toggle="tab" href="#app" role="tab" aria-controls="app" aria-selected="false">
+                            <span class="icon"><i class="icon-screen-tablet"></i></span>
+                            <h3>Commerce<br> Apps</h3>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="hubstaff-tab" data-toggle="tab" href="#hubstaff" role="tab" aria-controls="hubstaff" aria-selected="false">
+                            <span class="icon"><i class="icon-graduation"></i></span>
+                            <h3>Hubstaff<br> App</h3>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="engine-tab" data-toggle="tab" href="#engine" role="tab" aria-controls="engine" aria-selected="false">
+                            <span class="icon"><i class="icon-refresh"></i></span>
+                            <h3>Automation<br> Engine</h3>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="business-tab" data-toggle="tab" href="#business" role="tab" aria-controls="business" aria-selected="false">
+                            <span class="icon"><i class="icon-bulb"></i></span>
+                            <h3>Business<br> Intellegence</h3>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        @endsection
