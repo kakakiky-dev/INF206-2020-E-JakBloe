e1be8f29ac7f7f0f02815c281488f1f4bc325548
Nazla Amelia <nazla.a@mhs.unsyiah.ac.id>
2020-05-14 13:56:27 +0700

    Set routes
    Mengatur route halaman pada web jakbloe

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/home', function () {
+    return view('home');
+});
 Route::get('/about', function () {
     return view('about');
+});
+Route::get('/privacy', function () {
+    return view('privacy');
+});
+Route::get('/disclaimer', function () {
+    return view('disclaimer');
 });
 
 
