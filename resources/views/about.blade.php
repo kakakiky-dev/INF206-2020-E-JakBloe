9504d2b7be5f2f3a615ec7843825aff75f98362c
Nazla Amelia <nazla.a@mhs.unsyiah.ac.id>
2020-05-14 17:57:57 +0700

    Update About page
    Menambahkan css untuk  tampilan halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,164 +1,51 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <title>About JakBloe</title>
-</head>
-<body>
-    <img src="../public/img/jakbloe.png"/> 
-    <h1>About JakBloe</h1>
-    <p> JabBloe adalah website jual beli antara pedagang dan supplier.</p>
-    <p> Para pedagang dapat membeli barang secara bersamaan dengan pedagang lainnyamendapatkan harga yang lebih murah</p>
-    <br>
-    <h1>JakBloe Team</h1>
+@extends('master')
 
-    
-</body>
-</html>
- -->
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <title> About JakBloe</title>
-  </head>
-  <body>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
- 
- 
- <div class="container">
-    <div class="row">
-        <div class="col text-center">
-           <h1> About Jakbloe </h1>
-        </div>
-    </div>
-    <br>
-    
-    <div class="row">
-        <div class="col text-center">
-        <p>JakBloe adalah sebuah website jual beli online antara para pedagang dengan supplier. Pedagang dapat melakukan pembelian secara bersamaan dengan pedagang lainnya dalam jumlah yang besar. JakBloe memudahkan para pedagang berbelanja dengan harga yang lebih murah. Pada website ini, pedagang dan supplier membuat account untuk mengakses fitur lengkap. </p>
-        </div>
-        <!-- <div class="col">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, ratione architecto unde ipsa ex necessitatibus nemo sequi placeat eum possimus veritatis ut repellendus magni, harum nam sit. Repudiandae, illum officia.</p>
-        </div> -->
-    </div>
- </div> 
- <br>
-
- <div class="container">
-    <div class="row">
-        <div class="col text-center">
-            <h1> JakBloe Team</h1>
-        </div>
-    </div>
-    <br>
-
-    <div class="row">
-        <div class="col">
-                <img src="/img/AboutJb/nazla.jpeg" class="rounded-circle" width="250">
-                    <p> Nazla Amelia</p>
-        </div>
-
-        <div class="col">
-                <img src="/img/AboutJb/kiki.jpeg" class="rounded-circle" width="250">
-                    <p> M. Rizki Riswandi</p>
-        </div>
-
-        <div class="col">
-                <img src="/img/AboutJb/algi.jpeg" class="rounded-circle" width="250">
-                    <p> Muhammad Algihffari</p>
-        </div>
-    </div>
-    <br>
-    <div class="row">
-        <div class="col">
-                <img src="/img/AboutJb/nazira.jpeg" class="rounded-circle" width="250">
-                    <p> Nazira Rezky Afifah</p>
-        </div>
-
-        <div class="col">
-                <img src="/img/AboutJb/ari.jpeg" class="rounded-circle" width="250">
-                    <p> Arie Maulana</p>
-        </div>
-
-        <div class="col">
-                <img src="/img/AboutJb/avky.jpeg" class="rounded-circle" width="250">
-                    <p> Alvin Rizky</p>
-        </div>
-    </div>
-    <!-- <div class="row">
-        <div class="col">
-            <div class="card" >
-                <img src="/img/AboutJb/nazla.jpeg" class="card-img-top">
-                <div class="card-body">
-                    <p class="card-text">Nazla Amelia</p>
+@section('deskripsi')
+<section class="breadcrumb_area">
+            <img class="breadcrumb_shap" src="img/breadcrumb/banner_bg.png" alt="">
+            <div class="container">
+                <div class="breadcrumb_content text-center">
+                    <h1 class="f_p f_700 f_size_50 w_color l_height50 mb_20">About Us</h1>
+                    <p class="f_400 w_color f_size_16 l_height26">Halaman informasi tentang Website JakBloe</p>
                 </div>
             </div>
-        </div>
-
-         <div class="col">
-            <div class="card" >
-                <img src="/img/AboutJb/kiki.jpeg" class="card-img-top" >
-                <div class="card-body">
-                    <p class="card-text">M. Rizki Riswandi</p>
+        </section>
+        <section class="payment_features_two sec_pad">
+            <div class="container">
+                <div class="sec_title text-center mb_70 wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                    <h2 class="f_p f_size_30 l_height50 f_700 t_color">Website JakBloe</h2>
+                    <p class="f_400 f_size_18 l_height34">Jakbloe adalah sebuah website sebagai wadah untuk membeli barang secara massal<br>Sehingga harga barang yang didapatkan menjadi lebih murah. Ayo segera bergabung bersama kami!</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="payment_features_item text-center">
+                            <img src="img/new/icon8.png" alt="">
+                            <h4>Jakbloe</h4>
+                            <p>Dengan menggunakan JakBloe, kamu bisa membeli stok barang secara massal sehingga mendapatkan harga yang lebih murah<br><br></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="payment_features_item text-center">
+                            <img src="img/new/icon7.png" alt="">
+                            <h4>Responsive</h4>
+                            <p>JakBloe merupakan website yang bisa digunakan semua perangkat yang anda miliki, karena website ini sangat responsive.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="payment_features_item text-center">
+                            <img src="img/new/icon1.png" alt="">
+                            <h4>User Friendly</h4>
+                            <p>Memiliki fitur yang sangat mudah untuk digunakan dan tidak membingungkan.<br><br><br><br></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="payment_features_item text-center">
+                            <img src="img/new/icon9.png" alt="">
+                            <h4>Modern Design</h4>
+                            <p>Tampilan website yang elegan akan membuat anda nyaman menggunakan layanan dari kami<br><br><br></p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-
-          <div class="col">
-            <div class="card" >
-                <img src="/img/AboutJb/algi.jpeg" class="card-img-top" a>
-                <div class="card-body">
-                    <p class="card-text">Muhammad Alghiffari</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <br>
-     <br>
-
-    <div class="row">
-        <div class="col">
-            <div class="card" >
-                <img src="/img/AboutJb/nazira.jpeg" class="card-img-top">
-                <div class="card-body">
-                    <p class="card-text">Nazira Rezky Afifah</p>
-                </div>
-            </div>
-        </div>
-
-         <div class="col">
-            <div class="card" >
-                <img src="/img/AboutJb/ari.jpeg" class="card-img-top" >
-                <div class="card-body">
-                    <p class="card-text">Arie Maulana</p>
-                </div>
-            </div>
-        </div>
-
-          <div class="col">
-            <div class="card" >
-                <img src="/img/AboutJb/avky.jpeg" class="card-img-top" a>
-                <div class="card-body">
-                    <p class="card-text">Alvin Rizky</p>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
-  </div>
-  </body>
-</html>
+        </section>
+        @endsection
