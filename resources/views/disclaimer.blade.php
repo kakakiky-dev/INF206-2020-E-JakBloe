c9d0d3fe99fd5508ac9d96638d545ea4d2f3182e
Arie Maulana <arie.m@mhs.unsyiah.ac.id>
2020-05-14 22:57:27 +0700

    Update disclaimer.blade.php

--- resources/views/disclaimer.blade.php
+++ resources/views/disclaimer.blade.php
@@ -15,7 +15,7 @@
 
 Seluruh informasi dalam website JakBloe ditujukan hanya sebagai informasi umum. Kami tidak menjamin tentang keakuratan, kepastian dan kebenaran seluruh informasi dan tutorial yang ada. Setiap tindakan yang anda lakukan berdasarkan informasi dari situs ini merupakan tanggung jawab anda sendiri. Kami tidak bertanggung jawab terhadap kerugian materi dan immateri yang ditimbulkan oleh informasi yang tersaji dalam situs ini.
 
-Dari situs JakBloe, terdapat beberapa link keluar untuk mengunjungi situs lain. Walaupun kami selalu berusaha untuk menyediakan link ke situs berkualitas, namun kami tidak bisa mengontrol isi dari situs tersebut. Pemilik situs tersebut bisa saja mengubah konten yang ada tanpa pemberitahuan. Dalam hal ini, www.matericoding.com juga tidak bertanggung jawab terhadap kerugian yang berasal dari situs web tersebut.
+Dari situs JakBloe, terdapat beberapa link keluar untuk mengunjungi situs lain. Walaupun kami selalu berusaha untuk menyediakan link ke situs berkualitas, namun kami tidak bisa mengontrol isi dari situs tersebut. Pemilik situs tersebut bisa saja mengubah konten yang ada tanpa pemberitahuan. Dalam hal ini, JakBloe juga tidak bertanggung jawab terhadap kerugian yang berasal dari situs web tersebut.
 
 Juga perlu diingat bahwa ketika mengunjungi situs lain, situs tersebut bisa saja memiliki kebijakan privasi dan penggunaan informasi yang berbeda. Pastikan anda membaca kebijakan privasi dari situs tersebut dan juga aturan penggunaan informasi sebelum memberikan informasi apapun.
 
