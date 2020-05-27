3ef5a37f2b19956d0b4e51a01043ddf62df2c22b
Nazla Amelia <nazla.a@mhs.unsyiah.ac.id>
2020-05-27 23:49:40 +0700

    Updtae tampilan post
    Memperbaharui tampilan untuk membuat postingan

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,31 +1,48 @@
 @extends('master')
 
 @section('deskripsi')
-    <div class="container col-6">
-        <fieldset>
-            <form class="form-pesan" method="post">
-            @csrf
-                <h3 align="center"><br><br> Lengkapi Data Berikut <br> Untuk Membuat Postingan Baru <br><br></h1>
-                <label  for="Nama Pemesanan" class="sr-only">Nama Pemesan</label>
-                <input type="text" name="Nama Pemesanan" id="Nama Pemesanan" class="form-control" placeholder="Nama Pemesanan" required>
-                <br>
-                <label for="Judul Postingan" class="sr-only">Judul Postingan</label>
-                <input type="text" name="Judul Postingan" id="Judul Postingan" class="form-control" placeholder="Judul Postingan" required>
-                <br>
-                <label for="Target Pembelian (Kg)" class="sr-only">Target Pembelian (Kg)</label>
-                <input type="numeric" name="Target Pembelian" id="Target Pembelian" class="form-control" placeholder="Target Pembelian (Kg)" required autofocus>
-                <br>
-                <label for="Harga" class="sr-only">Harga</label>
-                <input type="numeric" name="Harga" id="Harga" class="form-control" placeholder="Harga" required>
-                <br>
-                <label  for="Jenis Barang" class="sr-only">Jenis Barang</label>
-                <input type="text" name="Jenis Barang" id="Jenis Barang" class="form-control" placeholder="Jenis Barang" required>
-                <br> <br> 
-                <p align="center">Masukkan Gambar Postingan <br> Max 5mb</p>
-                <br>
-            <button class="btn btn-lg btn-success btn-block" type="submit" style="border-radius: 40px; width: 300px; padding: 10px;margin: 5px auto;"
-                >Buat Postingan</button>
-        </fieldset>
-    </form>
-        </div>
+<section class="breadcrumb_area">
+            <img class="breadcrumb_shap" src="img/breadcrumb/banner_bg.png" alt="">
+            <div class="container">
+                <div class="breadcrumb_content text-center">
+                    <h1 class="f_p f_700 f_size_50 w_color l_height50 mb_20">Buat Postingan</h1>
+                    <p class="f_400 w_color f_size_16 l_height26">Isi Form Berikut Dengan Data Yang Benar</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="payment_features_two sec_pad">
+            <div class="container">
+                <div class="sign_info">
+                <div class="sec_title text-center mb_70 wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                    <h2 class="f_p f_size_30 l_height50 f_700 t_color">Buat Postingan</h2>
+                    <p class="f_400 f_size_18 l_height34">Lengkapi Data Berikut<br>Untuk Membuat Postingan Baru </p>
+                </div>
+                <fieldset>
+                                <form action="#" class="login-form sign-in-form" method="post">
+                                    @csrf
+                                    <div class="form-group text_box">
+                                        <label class="f_p text_c f_400 sr-only">Nama</label>
+                                        <input type="text" required="required" name="Nama" id="Nama" placeholder="Nama">
+                                    </div>
+                                    <div class="form-group text_box">
+                                        <label class="f_p text_c f_400 sr-only">Judul Postingan</label>
+                                        <input type="text" required="required" name="Judul" id="Judul" placeholder="Judul Postingan">
+                                    </div>
+                                    <div class="form-group text_box">
+                                        <label class="f_p text_c f_400 sr-only">Target Pembelian</label>
+                                        <input type="number" required="required" name="Target" id="Target" placeholder="Target Pembelian (kg)">
+                                    </div>
+                                    <div class="form-group text_box">
+                                        <label class="f_p text_c f_400 sr-only">Harga</label>
+                                        <input type="number" required="required" name="Harga" id="Harga" placeholder="Harga Barang / kg">
+                                    </div>
+                                    <div class="form-group text_box">
+                                    <input class="form-group text_box" type="file" id="file"> Masukkan Gambar Postingan (Max 5 Mb):
+                                    </div>
+                                        <button class="btn btn-lg btn-success btn-block" type="submit" style="border-radius: 40px; width: 300px; padding: 10px;margin: 5px auto;">Buat Postingan</button>
+                                    </form>
+                    </fieldset>
+                </div>
+</section>
 @endsection
