dca4b56e8914f55d89bfbc462e33825474448c2c
Nazla Amelia <nazla.a@mhs.unsyiah.ac.id>
2020-05-29 22:39:42 +0700

    Update Profil Team
    Menambahkan foto dan profil team pada halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,11 +6,7 @@
             <div class="container">
                 <div class="breadcrumb_content text-center">
                     <h1 class="f_p f_700 f_size_50 w_color l_height50 mb_20">About Us</h1>
-                    <p class="f_400 w_color f_size_16 l_height26">  JakBloe adalah sebuah website jual beli online antara para pedagang dengan supplier
-                    <br>
-                    PPedagang dapat melakukan pembelian secara bersamaan dengan pedagang lainnya dalam jumlah yang besar
-                    <br>
-                    Pada website ini, pedagang dan supplier membuat account untuk mengakses fitur lengkap. </p>
+                    <p class="f_400 w_color f_size_16 l_height26">Tentang Website JakBloe</p>
                 </div>
             </div>
         </section>
@@ -18,21 +14,25 @@
             <div class="container">
                 <div class="sec_title text-center mb_70 wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
                     <h2 class="f_p f_size_30 l_height50 f_700 t_color">Website JakBloe</h2>
-                    <p class="f_400 f_size_18 l_height34">Jakbloe adalah sebuah website sebagai wadah untuk membeli barang secara massal<br>Sehingga harga barang yang didapatkan menjadi lebih murah. Ayo segera bergabung bersama kami!</p>
+                    <p class="f_400 f_size_18 l_height34">JakBloe adalah sebuah website jual beli online antara para pedagang dengan supplier
+                    <br>
+                    PPedagang dapat melakukan pembelian secara bersamaan dengan pedagang lainnya dalam jumlah yang besar
+                    <br>
+                    Pada website ini, pedagang dan supplier membuat account untuk mengakses fitur lengkap.
                 </div>
                 <div class="row">
                     <div class="col-lg-3 col-sm-6">
                         <div class="payment_features_item text-center">
-                            <img src="img/new/icon8.png" alt="">
+                            <img src="img/new/icon2.png" alt="">
                             <h4>Jakbloe</h4>
-                            <p>Dengan menggunakan JakBloe, kamu bisa membeli stok barang secara massal sehingga mendapatkan harga yang lebih murah<br><br></p>
+                            <p>Dengan menggunakan JakBloe, kamu bisa membeli stok barang secara massal sehingga mendapatkan harga yang lebih murah</p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="payment_features_item text-center">
                             <img src="img/new/icon7.png" alt="">
                             <h4>Responsive</h4>
-                            <p>JakBloe merupakan website yang bisa digunakan semua perangkat yang anda miliki, karena website ini sangat responsive.</p>
+                            <p>JakBloe merupakan website yang bisa digunakan semua perangkat yang anda miliki, karena website ini sangat responsive.<br><br></p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
@@ -52,4 +52,193 @@
                 </div>
             </div>
         </section>
+
+        </section>
+        <section class="experts_team_area sec_pad">
+        <div class="container">
+            <div class="sec_title text-center mb_70">
+                <h2 class="f_p f_size_30 l_height30 f_700 t_color3 mb_20">JakBloe Expert Team</h2>
+                <p class="f_400 f_size_16">Website JakBloe ini dibangun dengan penuh cinta<br> Oleh kami para web developer Informatika Unsyiah</p>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="ex_team_item">
+                        <img src="img/kiky.png" alt="">
+                        <div class="team_content">
+                            <a href="#"><h3 class="f_p f_size_16 f_600 t_color3">M. Rizki Riswandi</h3></a>
+                            <h5>Frontend & Backend Developer</h5>
+                        </div>
+                        <div class="hover_content">
+                            <div class="n_hover_content">
+                                <ul class="list-unstyled">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                                <div class="br"></div>
+                                <a href="#"><h3 class="f_p f_size_16 f_600 w_color">M. Rizki Riswandi</h3></a>
+                                <h5>Frontend & Backend Developer</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="ex_team_item">
+                        <img src="img/algi.png" alt="">
+                        <div class="team_content">
+                            <a href="#"><h3 class="f_p f_size_16 f_600 t_color3">Muhammad Al Ghifari</h3></a>
+                            <h5>Database & Backend Developer</h5>
+                        </div>
+                        <div class="hover_content">
+                            <div class="n_hover_content">
+                                <ul class="list-unstyled">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                                <div class="br"></div>
+                                <a href="#"><h3 class="f_p f_size_16 f_600 w_color">Muhammad Al Ghifari</h3></a>
+                                <h5>Database & Backend Developer</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="ex_team_item">
+                        <img src="img/lala.png" alt="">
+                        <div class="team_content">
+                            <a href="#"><h3 class="f_p f_size_16 f_600 t_color3">Nazla Amelia</h3></a>
+                            <h5>Backend Developer</h5>
+                        </div>
+                        <div class="hover_content">
+                            <div class="n_hover_content">
+                                <ul class="list-unstyled">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                                <div class="br"></div>
+                                <a href="#"><h3 class="f_p f_size_16 f_600 w_color">Nazla Amelia</h3></a>
+                                <h5>Backend Developer</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="ex_team_item">
+                        <img src="img/zira.png" alt="">
+                        <div class="team_content">
+                            <a href="#"><h3 class="f_p f_size_16 f_600 t_color3">Nazira Rezky Afifah</h3></a>
+                            <h5>Backend Developer</h5>
+                        </div>
+                        <div class="hover_content">
+                            <div class="n_hover_content">
+                                <ul class="list-unstyled">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                                <div class="br"></div>
+                                <a href="#"><h3 class="f_p f_size_16 f_600 w_color">Nazira Rezky Afifah</h3></a>
+                                <h5>Backend Developer</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="ex_team_item">
+                        <img src="img/who.png" alt="">
+                        <div class="team_content">
+                            <a href="#"><h3 class="f_p f_size_16 f_600 t_color3">Unknown</h3></a>
+                            <h5>Unknown</h5>
+                        </div>
+                        <div class="hover_content">
+                            <div class="n_hover_content">
+                                <ul class="list-unstyled">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                                <div class="br"></div>
+                                <a href="#"><h3 class="f_p f_size_16 f_600 w_color">Unknown</h3></a>
+                                <h5>Unknown</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="ex_team_item">
+                        <img src="img/ari.png" alt="">
+                        <div class="team_content">
+                            <a href="#"><h3 class="f_p f_size_16 f_600 t_color3">Arie Maulana</h3></a>
+                            <h5>Frontend Developer</h5>
+                        </div>
+                        <div class="hover_content">
+                            <div class="n_hover_content">
+                                <ul class="list-unstyled">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                                <div class="br"></div>
+                                <a href="#"><h3 class="f_p f_size_16 f_600 w_color">Arie Maulana</h3></a>
+                                <h5>Frontend Developer</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="ex_team_item">
+                        <img src="img/alvin.png" alt="">
+                        <div class="team_content">
+                            <a href="#"><h3 class="f_p f_size_16 f_600 t_color3">Alvin Rizky</h3></a>
+                            <h5>Frontend Developer</h5>
+                        </div>
+                        <div class="hover_content">
+                            <div class="n_hover_content">
+                                <ul class="list-unstyled">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                                <div class="br"></div>
+                                <a href="#"><h3 class="f_p f_size_16 f_600 w_color">Alvin Rizky</h3></a>
+                                <h5>Frontend Developer</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="ex_team_item">
+                        <img src="img/who.png" alt="">
+                        <div class="team_content">
+                            <a href="#"><h3 class="f_p f_size_16 f_600 t_color3">Unknown</h3></a>
+                            <h5>Unknown</h5>
+                        </div>
+                        <div class="hover_content">
+                            <div class="n_hover_content">
+                                <ul class="list-unstyled">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                                <div class="br"></div>
+                                <a href="#"><h3 class="f_p f_size_16 f_600 w_color">Unknown</h3></a>
+                                <h5>Unknown</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
         @endsection
