374efb25eb7d7783cc0edcf40a8ed266bd0b43ae
alghifari3 <ghifari.13@mhs.unsyiah.ac.id>
2020-05-30 15:13:21 +0700

    membuat backend proses pembayaran dan menambahkan view daftar pembeli
    Menambahkan beberapa fungsi yang berhubungan dengan penimpanan data dalam database, dan membuat backend fungsi pembayaran

---
+++ app/konfirmasi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class konfirmasi extends Model
+{
+    protected $table='konfirmasi_gambar';
+    protected $fillable=['id','gambar'];
+}
