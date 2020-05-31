acae63be3f2fa50728a6d82b848772f22c7fff17
M. Rizki Riswandi <mrizki.r@mhs.unsyiah.ac.id>
2020-05-31 17:04:20 +0700

    Update Profile Page
    Menambahkan fitur postingan yang telah dibuat oleh user

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -65,4 +65,58 @@
                 </div>
             </div>
         </section>
+
+        <section class="shop_list_area sec_pad">
+            <div class="container">
+			<div class="sec_title mb_70 wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+			<h2 class="f_p f_size_30 l_height40 f_600 t_color text-center">My Post</h2>
+			<p class="f_400 t_color l_height28 wow fadeInRight text-center" data-wow-delay="0.4s">Berikut adalah postingan yang telah kamu buat</p>
+			</div>
+			@foreach($isi as $i)
+                <div class="row shop_list_item">
+                    <div class="col-md-5">
+                        <div class="shop_list_img">
+                            <img class="img-fluid" src="img/gambar/{{$i->file}}" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-7 single_product_item mt-0">
+					<div class="single_pr_details text-left">
+						<a href="#" class="s_list_title">
+							<h3 class="f_p f_500 f_size_22">{{$i->Judul}}</h3>
+							<p class="f_p f_150 f_size_12">Dibuat oleh: {{$i->Nama}} - {{$i->created_at}}</p>
+						</a>
+						<!-- <div class="ratting">
+							<a href="#"></a>
+						</div> -->
+						<div class="price"><span class="woocommerce-Price-amount amount">
+                            <span class="woocommerce-Price-currencySymbol">Target {{$i->Target}} </span>KG</span>	<ins><span class="woocommerce-Price-amount amount">
+                                <span class="woocommerce-Price-currencySymbol"></span>Harga Rp {{$i->Harga}}/kg |</span></ins>
+						</div>
+						<br>
+						<div class="row pr_button">
+							<form action="/pembayaran" method="get"><button type="submit" name="search" value="{{$i->id}}" class="cart_btn">Beli sekarang</button></form>&nbsp;&nbsp;<form action="/pembeli" method="get"><button type="submit" name="search" value="{{$i->id}}" class="cart_btn">Daftar Pembeli</button></form>
+						</div>&nbsp;
+						@php
+							$pembelis  =DB::table('pembeli')->where('idBarang', '=', $i->id)->get();
+							$total =0;
+							@endphp
+						@foreach ($pembelis as $pembeli)
+						@php
+								$total+=$pembeli->dibeli;
+								@endphp
+						@endforeach
+						@php
+							$value = ($total - 0) * (100 - 0) / ($i->Target - 0) + 0
+							
+						@endphp
+						<div class="progress">
+						<div class="progress-bar bg-success" role="progressbar" style="width: {{$value}}%" aria-valuenow="{{$value}}" aria-valuemin="0" aria-valuemax="100"></div>
+					</div>
+					</div>
+                    </div>
+                </div>
+				@endforeach
+            </div>
+        </section>
+
         @endsection
